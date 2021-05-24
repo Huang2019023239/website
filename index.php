5328aa7647278a3f28c297b7a7d924e2190d862c
Huang2019023239 <55315889+Huang2019023239@users.noreply.github.com>
2021-05-24 10:47:23 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <?php
 $host='127.0.0.1';
 $username='root';
-$password='2019023239';
+$password='xuehao';
 $dbname='db_book_php_15';
 $mysqli = mysqli_connect($host, $username, $password,$dbname);
 $query = "select count(*) as total from usercount";
